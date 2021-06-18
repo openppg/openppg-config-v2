618c7bc1acf5b4fb1bf8af8d54e649bc6aed8ed6
Zach Whitehead <zjwhitehead@users.noreply.github.com>
2021-06-18 15:11:02 -0500

    build

--- docs/js/configurator/sp140.js
+++ docs/js/configurator/sp140.js
@@ -14,7 +14,7 @@
     if ('usb' in navigator) {
       console.log('has WebUSB support');
     } else {
-      alert('WebUSB not supported: Please use Google Chrome');
+      alert('WebUSB not supported: Please use Chrome on desktop or Android');
       $('#connect').prop('disabled', true);
       $('#connect').html('Not Supported');
       return;
@@ -24,25 +24,24 @@
       updateLocalPressure();
     });
 
-    console.log(geoSupported());
     // listen for form input changes and save them to the device
     $('#frm-config input').on('change', function () {
       var orientation = $('input[name=orientation]:checked', '#frm-config').val();
       var baro_calibration = $('input#seaPressureInput').val();
       var min_batt_v = $('input#minBattInput').val();
       var max_batt_v = $('input#maxBattInput').val();
-      var metric_temp = $('#units-temp').prop('checked');
       var metric_alt = $('#units-alt').prop('checked');
+      var performance_mode = $('#performance-sport').prop('checked') ? 1 : 0;
 
       var usb_json = {
-        'major_v': 4,
-        'minor_v': 1,
+        'major_v': 5,
+        'minor_v': 0,
         'screen_rot': orientation,
         'sea_pressure': parseFloat(baro_calibration),
-        'metric_temp': metric_temp,
         'metric_alt': metric_alt,
         'min_batt_v': min_batt_v,
         'max_batt_v': max_batt_v,
+        'performance_mode': performance_mode,
       }
       console.log('sending', usb_json);
       sendJSON(usb_json);
@@ -67,25 +66,26 @@
         port.onReceive = data => {
           let textDecoder = new TextDecoder();
           var usb_input = textDecoder.decode(data);
-          if (usb_input.length < 5) { return };
+          if (usb_input.length < 5) { return }
           var usb_parsed = JSON.parse(usb_input); // TODO figure out why empty data is sent
           $('#armedTime').text(display(usb_parsed['armed_time']));
           $('#deviceId').text(usb_parsed['device_id']);
           $('#versionMajor').text(usb_parsed['major_v']);
           $('#versionMinor').text(usb_parsed['minor_v']);
-          $('#orientation-lh').prop('checked', usb_parsed['screen_rot'] == 2);
-          $('#orientation-rh').prop('checked', usb_parsed['screen_rot'] == 0);
-          $('#units-temp').prop('checked', usb_parsed['metric_temp']);
+          $('#orientation-lh').prop('checked', usb_parsed['screen_rot'] == 3);
+          $('#orientation-rh').prop('checked', usb_parsed['screen_rot'] == 1);
           $('#units-alt').prop('checked', usb_parsed['metric_alt']);
           $('#seaPressureInput').val(usb_parsed['sea_pressure']);
-          $('#minBattInput').val(usb_parsed['min_batt_v']);
-          $('#maxBattInput').val(usb_parsed['max_batt_v']);
-          console.log('received', usb_input);
+          $('#performance-chill').prop('checked', usb_parsed['performance_mode'] == 0);
+          $('#performance-sport').prop('checked', usb_parsed['performance_mode'] == 1);
+          Rollbar.info('Synced-SP140', usb_parsed);
         };
         port.onReceiveError = error => {
+          Rollbar.warn(error);
           console.error(error);
         };
       }, error => {
+        Rollbar.warn(error);
         displayError(error)
       });
     }
@@ -136,6 +136,5 @@
     function sendJSON(usb_json) {
       port.send(new TextEncoder('utf-8').encode(JSON.stringify(usb_json)));
     }
-
   });
 })();
